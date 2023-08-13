d240189d566dafa71592edeb6bf5a7f0edee40ca
smitpatelx <smitbpatel1999@gmail.com>
2023-08-13 15:37:23 -0500

    Improved DX and fixed build error.

--- src/lib/helpers.ts
+++ src/lib/helpers.ts
@@ -1,6 +1,19 @@
 import { Unit } from '@/interfaces/beers';
 import type { BoilVolume } from '@/interfaces/beers';
 
+export const randomAlpha = (): string => {
+  const length = 9;
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  const charactersLength = characters.length;
+
+  let result = '';
+
+  for (let i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
+};
+
 const UnitMap = {
   [Unit.Litres]: 'L',
   [Unit.Celsius]: '°C',
